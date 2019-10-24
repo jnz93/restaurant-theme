dfd7b341c57b2c42ce650bce44029fb88c6df900
jz93 <box@unitycode.tech>
2019-10-24 15:47:53 -0300

    Criação dos arquivos, diretórios e arquitetura do css/sass

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sassCompile', function()
+{
+    return gulp
+        .src('')
+        .pipe(sass)
+        .pipe(gulp.dest)
+})
