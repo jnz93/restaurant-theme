99536db9e12b67d10f15f73aebd1f0dfffe07a6b
jz93 <box@unitycode.tech>
2019-11-01 17:50:38 -0300

    Fix syntax error

--- template-parts/content/content-excerpt.php
+++ template-parts/content/content-excerpt.php
@@ -13,11 +13,11 @@
 <article id="post-<?php the_ID(); ?>" class="<?php post_class() ?>">
     <header class="entry-header">
         <?php 
-        if (is_stick() && is_home() && !is_paged())
+        if (is_home() && !is_paged())
         {
             printf('<span class="sticky-post">%s</span>', _x('Featured', 'post', 'TEXT_DOMAIN'));
         }
-        the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink() ) ), '</a></h2>'));
+        the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink() ) ), '</a></h2>');
         ?>
     </header> <!-- .entry-header -->
 
